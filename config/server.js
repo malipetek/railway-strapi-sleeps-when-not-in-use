177e47feb946861cdf494f01aacf48302c45cee3
m.ali petek <malipetek@gmail.com>
2023-03-20 06:36:24 +0300

    try fixing strapi config again

--- config/server.js
+++ config/server.js
@@ -2,9 +2,7 @@ module.exports = ({ env }) => ({
   host: '0.0.0.0',
   port: 1337,
   proxy: true, // This makes Strapi (and therefore the Koa instance) aware of the proxy.
-  url: process.env.RAILWAY_STATIC_URL || "http://localhost:3000/",
   admin: {
-    url: (process.env.RAILWAY_STATIC_URL || "http://localhost:3000") + "/admin",
     autoOpen: false
   },
   app: {
